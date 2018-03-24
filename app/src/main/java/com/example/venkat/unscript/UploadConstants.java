fb6e76c268563aef0663f9ed110ef10305c6cdea
SlavCzar <shantanughar11@gmail.com>
2018-03-24 17:42:06 +0530

    Dependencies Added

---
+++ app/src/main/java/com/example/venkat/unscript/UploadConstants.java
@@ -0,0 +1,10 @@
+package com.example.venkat.unscript;
+
+/**
+ * Created by shantanu on 24/3/18.
+ */
+
+public class UploadConstants {
+    public static final String STORAGE_PATH_UPLOADS = "uploads/";
+    public static final String DATABASE_PATH_UPLOADS = "uploads";
+}
