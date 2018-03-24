aaae905247b31f57227256bae8cafd98ef186a06
Shantanu <shantanu.ghar16@siesgst.ac.in>
2018-03-24 16:58:05 +0530

    Placement Fragment created

---
+++ app/src/main/java/com/example/venkat/unscript/PlacementFragment.java
@@ -0,0 +1,39 @@
+package com.example.venkat.unscript;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+/**
+ * Created by shantanu on 24/3/18.
+ */
+
+public class PlacementFragment extends Fragment{
+
+    public PlacementFragment()
+    {
+
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_placement,container,false);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        Button uploadLetter=(Button)getView().findViewById(R.id.upload_offer_letter);
+        uploadLetter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+    }
+}
