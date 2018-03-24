b20ad40ac2a056830e68100f0b82627839d9d439
SlavCzar <shantanughar11@gmail.com>
2018-03-25 01:51:13 +0530

    Circulars model class completed

--- app/src/main/java/com/example/venkat/unscript/Circulars.java
+++ app/src/main/java/com/example/venkat/unscript/Circulars.java
@@ -5,4 +5,37 @@ package com.example.venkat.unscript;
  */
 
 public class Circulars {
+    String title,description,date,time;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
 }
