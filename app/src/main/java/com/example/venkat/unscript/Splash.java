006426afa8cfeaa0c411aedace628fc3cb7d3f9b
Venkhatesh <venkhatesharunachalam@gmail.com>
2018-03-25 09:13:59 +0530

    Splash_commit

--- app/src/main/java/com/example/venkat/unscript/Splash.java
+++ app/src/main/java/com/example/venkat/unscript/Splash.java
@@ -2,14 +2,18 @@ package com.example.venkat.unscript;
 
 import android.content.Intent;
 import android.os.Handler;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class Splash extends AppCompatActivity {
     Handler handler;
+    ActionBar actionBar ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        actionBar = getSupportActionBar();
+        actionBar.hide();
         setContentView(R.layout.activity_splash);
 
         handler = new Handler();
